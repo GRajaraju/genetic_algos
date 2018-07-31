03836bb16828fa03aa16bdb0ee701611783f308e
jungleecode <jungleecode@users.noreply.github.com>
2018-07-31 23:34:21 +0530

    Update first_go.py

--- first_go.py
+++ first_go.py
@@ -1,3 +1,14 @@
 
-
 import random
+
+population = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ '
+target = 'guess me'
+
+def generate_random_word(target_length):
+  
+  guess = []
+   while len(guess) < target_length:  # using while loop in case the target word is longer than the population.
+      size_of_guess = min(target_length - len(guess), len(population))
+      guess.extend(random.sample(population,size_of_guess))
+   return "".join(guess)
+
