3364137a3fe26d16e515130ea46b79317c6a9879
jungleecode <jungleecode@users.noreply.github.com>
2018-07-31 23:41:15 +0530

    Update first_go.py

--- first_go.py
+++ first_go.py
@@ -8,7 +8,7 @@ def generate_random_word(target_length):
   
   guess = []
    while len(guess) < target_length:  # using while loop in case the target word is longer than the population.
-      size_of_guess = min(target_length - len(guess), len(population))
+      size_of_guess = min(target_length - len(guess), len(population)) # this would help keeping the guessed word size within the size of population.
       guess.extend(random.sample(population,size_of_guess))
    return "".join(guess)
 
